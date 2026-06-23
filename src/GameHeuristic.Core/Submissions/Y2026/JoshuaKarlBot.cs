f9a0dffc72dbc2745d6117f957f891c0f36b756c
Joshua Elliott <255935@esher.ac.uk>
2026-06-23 11:55:54 +0100

    V2, can beat all baseline heuristics as either red or yellow

--- src/GameHeuristic.Core/Submissions/Y2026/JoshuaKarlBot.cs
+++ src/GameHeuristic.Core/Submissions/Y2026/JoshuaKarlBot.cs
@@ -15,8 +15,107 @@ public class JoshuaKarlBot : IHeuristic
     public double Evaluate(Player[,] board, Player player)
     {
         double score = 0;
-
-
+        Player enemy = Player.None;
+        if (player == Player.Red)
+        {
+            enemy = Player.Yellow;
+        }
+        else
+        {
+            enemy = Player.Red;
+        }
+        int[,] bonus = new int[board.GetLength(0), board.GetLength(1)];
+        for (int a = 0; a < board.GetLength(0); a++)
+        {
+            for (int b = 0; b < board.GetLength(1); b++ )
+            {
+                if (board[a,b] == player)
+                {
+                    for (int c = -1; c <= 1; c++)
+                    {
+                        for (int d = -1; d <= 1; d++)
+                        {
+                            if ((a + c > 0 && a + c < board.GetLength(0)) && (b + d > 0 && b + d < board.GetLength(1)))
+                            {
+                                if (board[a + c, b + d] == player)
+                                {
+                                    score += 1;
+                                    bonus[a,b] += 1;
+                                    if ((a + (2 * c) > 0 && a + (3 * c) < board.GetLength(0)) && (b + (2 * d) > 0 && b + (3 * d) < board.GetLength(1)))
+                                    {
+                                        if (board[a + c + c, b + d + d] != enemy && (board[a + c + c + c, b + d + d + d] != enemy))
+                                        {
+                                            score += 2;
+                                        }
+                                    }
+                                }
+                                else if (board[a + c, b + d] == Player.None)
+                                {
+                                    score += 0.1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (int a = 0; a < board.GetLength(0); a++)
+        {
+            for (int b = 0; b < board.GetLength(1); b++)
+            {
+                if (board[a, b] == enemy)
+                {
+                    for (int c = -1; c <= 1; c++)
+                    {
+                        for (int d = -1; d <= 1; d++)
+                        {
+                            if ((a + c > 0 && a + c < board.GetLength(0)) && (b + d > 0 && b + d < board.GetLength(1)))
+                            {
+                                if (board[a + c, b + d] == enemy)
+                                {
+                                    score -= 0.9;
+                                    bonus[a, b] -= 1;
+                                    if ((a + (3 * c) > 0 && a + (3 * c) < board.GetLength(0)) && (b + (3 * d) > 0 && b + (3 * d) < board.GetLength(1)))
+                                    {
+                                        if ((board[a + c + c, b + d + d] != player) && (board[a + c + c + c, b + d + d + d] != player)) 
+                                        {
+                                            score -= 3;
+                                        }
+                                    }
+                                }
+                                else if (board[a + c, b + d] == Player.None)
+                                {
+                                    score -= 0.1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (int a = 0; a < board.GetLength(0); a++)
+        {
+            for (int b = 0; b < board.GetLength(1); b++)
+            {
+                switch(bonus[a,b])
+                {
+                    case 2:
+                        score += 1;
+                        break;
+                    case >2:
+                        score += 2;
+                        break;
+                    case -2:
+                        score -= 2;
+                        break;
+                    case < -2:
+                        score -= 3;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
 
 
 
