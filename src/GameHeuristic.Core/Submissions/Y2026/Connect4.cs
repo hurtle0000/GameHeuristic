8eb64a03428b44c1db880f6d23c02d3644af83d0
ESHER5517 <255517@esher.ac.uk>
2026-06-28 23:09:23 +0100

    Update Connect4.cs

--- src/GameHeuristic.Core/Submissions/Y2026/Connect4.cs
+++ src/GameHeuristic.Core/Submissions/Y2026/Connect4.cs
@@ -1,12 +1,11 @@
-namespace GameHeuristic.Core.Submissions.Experiment;
+namespace GameHeuristic.Core.Submissions.Y2026;
 
 /// <summary>
-/// Team Ilias
-/// Team George
+/// Team Ilias George
 /// </summary>
-public class IliasGeorge : IHeuristic
+public class Connect4_2 : IHeuristic
 {
-    public string Name => "IliasGeorge";
+    public string Name => "georges 🐷‼️ supa piggy";
     public double Evaluate(Player[,] board, Player player)
     {
         double score = 0;
@@ -47,6 +46,24 @@ public class IliasGeorge : IHeuristic
             }
         }
         
+        Player opponent =
+            player == Player.Red
+                ? Player.Yellow
+                : Player.Red;
+
+        // better high depth results, h2h but weaker leaderboard/tournament. not worth imo
+        // becomes an absolute H2H (6 depth and up) tank, but reduces lower depth gains
+        // to be most consistent, i simply had to choose to not gaf about single matches
+        // focusing on leaderboard and knockout instead. its not perfect which  i hate
+        
+        int centre = Board.Columns / 2;
+
+        for (int r = 0; r < Board.Rows; r++)
+        {
+            if (board[r, centre] == player) score += 100;
+            else score -= 9999;
+        }
+        
         return score;
     }
     private double EvaluateWindow(Player p1, Player p2, Player p3, Player p4, Player player, Player[,] board)
@@ -67,8 +84,33 @@ public class IliasGeorge : IHeuristic
 
         score = -opponentCount + playerCount;
         
-        if (playerCount == 3 && emptyCount == 1) score += 90;
-        if (opponentCount == 3 && emptyCount == 1) score -= 120;
+        if (playerCount == 3 && emptyCount == 1) score += 1000;
+        if (opponentCount == 3 && emptyCount == 1) score -= 12000;
+        
+        // tournament winner, reduces leaderboard score (NOT WORTH IMO)
+        // AFTER 2 hours i can confirm THIS IS NOT WORTH ANYMORE!!
+        // if (opponentCount == 0 && playerCount == 2) score += 1000;
+        int[] columnWeights =
+        {
+            1, 4, 8, 16, 8, 4, 1
+        };
+        
+        Player opponent =
+            player == Player.Red
+                ? Player.Yellow
+                : Player.Red;
+
+        for (int r = 0; r < Board.Rows; r++)
+        {
+            for (int c = 0; c < Board.Columns; c++)
+            {
+                if (board[r,c] == player)
+                    score += columnWeights[c];
+
+                if (board[r,c] == opponent)
+                    score -= columnWeights[c];
+            }
+        }
         
         return score;
 
