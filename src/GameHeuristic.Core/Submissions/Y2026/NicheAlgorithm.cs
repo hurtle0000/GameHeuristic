b851c24529098a8041e91944b584f77839646123
Oliver Hislop <255602@esher.ac.uk>
2026-06-29 08:47:51 +0100

    buns

--- src/GameHeuristic.Core/Submissions/Y2026/NicheAlgorithm.cs
+++ src/GameHeuristic.Core/Submissions/Y2026/NicheAlgorithm.cs
@@ -10,12 +10,28 @@ public class NicheAlgorithm : IHeuristic
 
     public double Evaluate(Player[,] board, Player player)
     {
-        if (board[5,3] == player)
+        Player enemyPlayer;
+        if (player == Player.Red)
+            enemyPlayer == Player.Yellow;
+        else
+            enemyPlayer = Player.Red;
+
+        if (board[5, 3] == player)
             return 5;
         else if (board[5, 3] == player || board[5, 4] == player)
             return 4;
 
-        
+        for (int r = 0; r < 3; r++)
+        {
+            for (int c = 0; c <= Board.Columns - 4; c++)
+            {
+                if (board[r,c + 1] == enemyPlayer,
+                    board[r,c + 2] == enemyPlayer,
+                    board[r, c + 3] == enemyPlayer)
+                return 10;
+            }
+        }
+
         return 0.0;
     }
 }
