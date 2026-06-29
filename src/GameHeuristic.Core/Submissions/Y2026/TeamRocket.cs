d509a98355eeb0bec0fff711da623998ea474a8f
GCE <gevans@esher.ac.uk>
2026-06-29 11:15:39 +0100

    merge test

---
+++ src/GameHeuristic.Core/Submissions/Y2026/TeamRocket.cs
@@ -0,0 +1,19 @@
+ using System;
+using GameHeuristic.Core;
+
+namespace GameHeuristic.Core.Submissions.Y2026;
+
+
+public class TeamRocket : IHeuristic
+{
+    public string Name => "TeamRocket";
+
+   
+    public double Evaluate(Player[,] board, Player player)
+    {
+
+        return 99.0d;
+    }
+
+ 
+}
